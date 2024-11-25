df44944721754b2677c0fc89d32d4a5e764b28d9
Maria Eduarda Nunes <95004623+Madu3304@users.noreply.github.com>
2024-11-25 10:16:38 -0300

    Add files via upload

---
+++ Atividade6/Atividade6/Produto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Atividade6
+{
+    class Produto
+    {
+        public string Nome { get; set; }
+        public double Produ { get; set; }
+
+        public Produto(String nome, double produ)
+        {
+            Nome = nome;
+            Produ = produ;
+        }
+
+        public override string ToString()
+        {
+            return Nome + "," + Produ.ToString("F2");
+        }
+    }
+}
---
+++ Atividade6/Atividade6/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using Atividade6;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Produto> list = new List<Produto>();
+
+            list.Add(new Produto("TV", 900.00));
+            list.Add(new Produto("TV 58", 1050.00));
+            list.Add(new Produto("TV", 980.00));
+            list.Add(new Produto("Celular", 967.00));
+            list.Add(new Produto("Conrole", 1000.00));
+            list.Add(new Produto("Tablet", 150.00));
+            list.Add(new Produto("HD", 5200.00));
+            list.Add(new Produto("Mouse", 234.00));
+            list.Add(new Produto("Mouse Pad", 80.00));
+
+            list.ForEach(UpdateProduto);
+            foreach( Produto p in list)
+            {
+                Console.WriteLine(p);
+            }
+
+        }
+
+        static void UpdateProduto(Produto p)
+        {
+            p.Produ += p.Produ * 0.1;
+        }
+    }
+}
