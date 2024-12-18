956842c0bd5acc0c3ea1735b61f5e4c71f62fe79
Maria Eduarda Nunes <95004623+Madu3304@users.noreply.github.com>
2024-12-18 09:24:19 -0300

    Add files via upload

---
+++ atividade10/CarteiraTest/CarteiraTest/Carteira.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarteiraTest
+{
+    public class Carteira
+    {
+        static void Main(string[] args)
+        {
+            Program car = new Program();
+
+            Console.WriteLine("O saldo é: " + car.Dinheiro);
+        }
+    }
+}
---
+++ atividade10/CarteiraTest/CarteiraTest/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Runtime.ConstrainedExecution;
+
+namespace CarteiraTest
+{
+     public class Program
+    {
+
+        //variavel 
+        private int dinheiro = 50;
+
+        public int Dinheiro { get; set; }
+        
+       
+        //metodo
+        public void Acrescentar(int valor)
+        {
+            //vai receber ela mesmo e o valor
+            dinheiro += valor;
+        }
+
+
+        //metodo de tirar 
+        public void Retirar(int valor)
+        {
+            //vai receber ela mesmo e tirar valor
+            dinheiro -= valor;
+
+        }
+
+        public int MostrarDinheiro()
+        {
+            return dinheiro;
+        }
+
+        public void AlterarDinheiro(int num)
+        {
+            dinheiro = num;
+        }
+
+    }
+
+
+}
